f00ffff093487a4c9282d9c4752ea6d1dfb48fd1
birds0501 <birds0501@naver.com>
2023-12-08 10:56:17 +0900

    언니로 변경

--- 01-언니가 간다/js/script.js
+++ 01-언니가 간다/js/script.js
@@ -2,7 +2,7 @@ $(function () {
   //대상을 변수로 저장
   const $window = $(window);
   const $document = $(document);
-  const $progressBar = $(".progress-bar");
+  const $girl = $(".girl");
 
   //크기를 구해오는 jQuery 메서드 : outerHeight()
   const windowHeight = $window.outerHeight();
@@ -10,19 +10,16 @@ $(function () {
   console.log(windowHeight, documentHeight);
   //스크롤 영역의 세로 크기
   const scrollHeight = documentHeight - windowHeight;
-  // console.log(scrollHeight);
 
   //스크롤이 발생하면
   $window.on("scroll", function () {
     //사용자의 (세로 )스크롤 값을 구해서
     let scrollTop = $(this).scrollTop();
-    // console.log(scrollTop);
 
     //비율을 구하자. 공식은  (대상 / 기준)*100
     let percent = (scrollTop / scrollHeight) * 100 + "%";
-    console.log(percent);
 
     //$progressBar 의 width로 적용(%)
-    $progressBar.css("width", percent);
+    $girl.css("left", percent);
   });
 });
